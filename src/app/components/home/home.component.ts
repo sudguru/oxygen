e75849c43ecdac0d9773b0954b1b3548e17b6c33
Sudeep Gurung <sudeepgurung.sg@gmail.com>
2018-08-09 18:51:29 +0545

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { ElectronService } from '../../providers/electron.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private electron: ElectronService) { }
+
+  ngOnInit() {
+  }
+
+  closeWindow() {
+    this.electron.window.close();
+  }
+
+  minimizeWindow() {
+    this.electron.window.minimize();
+  }
+
+  maximizeWindow() {
+    this.electron.window.maximize();
+  }
+
+  resizeWindow() {
+    this.electron.window.restore();
+  }
+
+}
